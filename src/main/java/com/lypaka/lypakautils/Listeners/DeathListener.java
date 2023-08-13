ee9e8d21b368066984f7247fb29ba148ff061e11
Lypaka <ryan96t@gmail.com>
2023-08-13 14:02:22 -0500

    And made that listener actually work

--- src/main/java/com/lypaka/lypakautils/Listeners/DeathListener.java
+++ src/main/java/com/lypaka/lypakautils/Listeners/DeathListener.java
@@ -1,18 +1,21 @@
 package com.lypaka.lypakautils.Listeners;
 
+import com.lypaka.lypakautils.LypakaUtils;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 import java.util.UUID;
 
 /**
  * Fixes a desync issue with player location upon being killed
  */
+@Mod.EventBusSubscriber(modid = LypakaUtils.MOD_ID)
 public class DeathListener {
 
     @SubscribeEvent
-    public void onRespawn (PlayerEvent.PlayerRespawnEvent event) {
+    public static void onRespawn (PlayerEvent.PlayerRespawnEvent event) {
 
         ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
         putTheBitchBackIfMissing(player);
